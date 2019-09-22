cfe5bf4e603f8043c635cfd51c2904b72d81a70a
ayerest <irisiseve@gmail.com>
2019-09-21 21:48:01 -0700

    finished react presentational components lab

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,28 @@
 // Code SimpleComponent Here
+import React, {Component} from 'react'
+
+class SimpleComponent extends Component {
+    constructor() {
+        super()
+        this.state = {
+            mood: 'happy'
+        }
+    }
+
+    handleClick = (event) => {
+        // debugger
+        let mood = ""
+        this.state.mood === "happy" ? mood = "sad" : mood = "happy"
+        this.setState({
+            mood: mood
+        }, () => console.log(this.state.mood))
+    }
+
+    render() {
+        return (
+            <div onClick={this.handleClick}>{this.state.mood}</div>
+        )
+    }
+}
+
+export default SimpleComponent
